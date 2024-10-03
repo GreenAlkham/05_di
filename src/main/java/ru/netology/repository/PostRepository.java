61fd7b3489a59339c408e9a21467d6434c407998
AlKham <greenowlkitty@gmail.com>
2024-10-03 20:36:34 +0500

    Edit di-annotation

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,27 +1,28 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
-// Stub
-public class PostRepository {
+@Repository
+public interface PostRepository {
 
-  private final HashMap<Long, Post> posts = new HashMap<>();
-  private final AtomicLong idPost = new AtomicLong();//для многопоточки
+  final HashMap<Long, Post> posts = new HashMap<>();
+  final AtomicLong idPost = new AtomicLong();//для многопоточки
 
-  public List<Post> all() {
+  public default List<Post> all() {
       return new ArrayList<>(posts.values());
     //return Collections.emptyList();
   }
 
-  public Optional<Post> getById(long id) {
+  public default Optional<Post> getById(long id) {
     return Optional.ofNullable(posts.get(id)); // ofNullable - чтобы не выбрасывалось исключение
    // return Optional.empty();
   }
 
-  public Post save(Post post) {
+  public default Post save(Post post) {
 if (post.getId() == 0) {
   post.setId(idPost.getAndIncrement());
   posts.put(idPost.getAndIncrement(), post);
@@ -34,7 +35,7 @@ if (post.getId() == 0) {
     return post;
   }
 
-  public void removeById(long id) {
+  public default void removeById(long id) {
 posts.remove(id);
   }
 }
